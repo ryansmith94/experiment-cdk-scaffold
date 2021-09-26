d1d0e90760e0c3d273b5df5378de0efe139da7ec
Ryan Smith <0ryansmith1994@gmail.com>
2021-09-26 14:14:36 +0100

    chore: Simplifies deployment code

--- readme.md
+++ readme.md
@@ -2,9 +2,8 @@
 
 1. Install dependencies with `npm ci`
 1. Build the code with `npm run build`
-1. Configure AWS with `aws configure` (a SysAdmin can help here)
-1. Bootstrap the CDK with `npm run bootstrap aws://{ACCOUNT_ID}/{REGION}` (a SysAdmin can help here)
-1. Synthesize the CDK with `npm run synth`
+1. Configure AWS with `aws configure`
+1. Bootstrap the CDK with `npm run bootstrap aws://{ACCOUNT_ID}/{REGION}`
 1. Deploy the code with `npm run deploy`
 1. You'll then get an endpoint to hit, just append `/service` and open in your browser
 1. It should say "hello"
--- src/deploy.ts
+++ src/deploy.ts
@@ -1,6 +1,22 @@
 // npx ts-node src/deploy.ts
 import * as cdk from '@aws-cdk/core';
-import { ServiceStack } from './service/service.cdk';
+import { Construct, Stack, StackProps } from '@aws-cdk/core';
+import { deployService } from './service/service.deploy';
 
-const scope = new cdk.App();
-new ServiceStack(scope, 'example');
+async function main() {
+  const scope = new cdk.App();
+  const service = await deployService();
+  class ServiceStack extends Stack {
+    constructor(scope: Construct, id: string, props?: StackProps) {
+      super(scope, id, props);
+      service(this);
+    }
+  }
+
+  new ServiceStack(scope, 'example');
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
